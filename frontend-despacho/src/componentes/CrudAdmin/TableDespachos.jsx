a73e0d862c29b0c77d9cb16c2da40ffd35c4bc15
Your Name <br.godoys@duocuc.cl>
2026-05-19 21:51:26 -0400

    Implementacion de seguridad con Reverse Proxy en Nginx

--- frontend-despacho/src/componentes/CrudAdmin/TableDespachos.jsx
+++ frontend-despacho/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://98.83.74.130:8081/api/v1/despachos", {
+      .get("/api/v1/despachos", {
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
